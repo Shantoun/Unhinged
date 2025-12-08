8c0f0bd127d2e5dda371509f3487f849467dd8c9
Shantoun <40128978+Shantoun@users.noreply.github.com>
2025-12-08 00:12:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-# import streamlit as st
 
-# if not st.user.is_logged_in:
-#     st.write("Not logged in")
-#     if st.button("Sign in with Google"):
-#         st.login("google")
-# else:
-#     st.write(f"User: {st.user.name}")
-#     st.write(f"Email: {st.user.email}")
-#     st.success("Logged in")
-    
-#     if st.button("Sign out"):
-#         st.logout()
 import streamlit as st
 import functions.authentification as auth
 
