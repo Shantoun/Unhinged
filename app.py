d46d34fb689cff41d016afbc3538c5360a1fee6f
Shantoun <40128978+Shantoun@users.noreply.github.com>
2025-12-08 18:07:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import streamlit as st
 import functions.authentification as auth
+import main.main as main
 
 
-def main_app(user_email):
-    st.set_page_config(layout="wide")
-    st.title("Unhinged")
-    st.caption("Analyze your game")
 
-    with st.sidebar:
-        if st.button("Sign Out", width="stretch"):
-            auth.sign_out()
+main()
 
-if "user_email" not in st.session_state:
-    st.session_state.user_email = None
+# def main_app(user_email):
+#     st.set_page_config(layout="wide")
+#     st.title("Unhinged")
+#     st.caption("Analyze your game")
+    
+#     with st.sidebar:
+#         if st.button("Sign Out", width="stretch"):
+#             auth.sign_out()
 
-if st.session_state.user_email:
-    main_app(st.session_state.user_email)
-else:
-    auth.auth_screen()
+    
+
+
+
+# if "user_email" not in st.session_state:
+#     st.session_state.user_email = None
+
+# if st.session_state.user_email:
+#     main_app(st.session_state.user_email)
+# else:
+#     auth.auth_screen()
