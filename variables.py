2e03375a22b4fcb1b1c8f5fb61266bcff4573ca9
Shantoun <40128978+Shantoun@users.noreply.github.com>
2025-12-10 17:53:23 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+####################################################### table names
+matches = "matches"
+
+
+
+
+####################################################### column names
+match_id = "match_id"
+match_timestamp = "match_timestamp"
+uid = "user_id"
