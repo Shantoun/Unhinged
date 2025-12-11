9558e0dfe1c4e32358ff91bab0292b32f5445b33
Shantoun <40128978+Shantoun@users.noreply.github.com>
2025-12-10 19:45:29 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,18 +1,21 @@
 ####################################################### json keys
-json_matches      = "matches"
-json_blocks       = "blocks"
+json_matches       = "matches"
+json_blocks        = "blocks"
+json_likes         = "like"
 
-json_match_event  = "match"     # nested array inside each match
-json_block_event  = "block"     # nested array inside each block
+json_match_event   = "match"
+json_block_event   = "block"
+json_like_event    = "like"      # inner like array
 
-json_timestamp    = "timestamp"
-json_match_id     = "match_id"
-json_block_type   = "block_type"
+json_timestamp     = "timestamp"
+json_block_type    = "block_type"
+json_like_comment  = "comment"
 
 
 ####################################################### table names
 table_matches = "matches"
 table_blocks  = "blocks"
+table_likes   = "likes"
 
 
 ####################################################### shared column names
@@ -28,3 +31,9 @@ col_match_timestamp = "match_timestamp"
 col_block_id        = "block_id"
 col_block_timestamp = "block_timestamp"
 col_block_type      = "block_type"
+
+
+####################################################### likes column names
+col_like_id         = "like_id"
+col_like_timestamp  = "like_timestamp"
+col_like_comment    = "comment"
