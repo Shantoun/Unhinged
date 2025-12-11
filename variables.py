3b6c5c0a0dbe04947791bc660fee8fc9fcda63dc
Shantoun <40128978+Shantoun@users.noreply.github.com>
2025-12-10 19:13:03 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,14 +1,30 @@
 ####################################################### json keys
-json_matches = "matches"
-json_match_id = "match_id"
-json_timestamp = "timestamp"
+json_matches      = "matches"
+json_blocks       = "blocks"
+
+json_match_event  = "match"     # nested array inside each match
+json_block_event  = "block"     # nested array inside each block
+
+json_timestamp    = "timestamp"
+json_match_id     = "match_id"
+json_block_type   = "block_type"
 
 
 ####################################################### table names
 table_matches = "matches"
+table_blocks  = "blocks"
 
 
-####################################################### column names
-col_match_id = "match_id"
+####################################################### shared column names
+col_user_id   = "user_id"
+col_match_id  = "match_id"
+
+
+####################################################### matches column names
 col_match_timestamp = "match_timestamp"
-col_user_id = "user_id"
+
+
+####################################################### blocks column names
+col_block_id        = "block_id"
+col_block_timestamp = "block_timestamp"
+col_block_type      = "block_type"
