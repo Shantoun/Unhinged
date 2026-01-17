61490f514a9de671eb22e61e1519044f9ac5f579
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-01-16 20:43:57 -0500

    Refactor JSON keys and table names in variables.py
    Updated JSON keys and table names for better clarity and organization.

--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
-####################################################### json keys
+####################################################### JSON Keys
 json_matches      = "matches"
 json_blocks       = "blocks"
 json_like_key     = "like"
@@ -22,8 +22,39 @@ json_prompt_media_url = "media_url"
 json_we_met = "did_meet_subject"
 json_my_type = "was_my_type"
 
+json_media_list     = "media"
+json_media_type     = "type"
+json_media_url      = "url"
+json_media_social   = "from_social_media"
+json_media_prompt = "prompt"
 
-####################################################### table names
+json_prompts        = "prompts"
+json_prompt_id      = "id"
+json_prompt_label   = "prompt"
+json_prompt_type    = "type"
+json_prompt_text    = "text"
+json_prompt_created = "created"
+json_prompt_updated = "user_updated"
+
+
+json_subscriptions        = "subscriptions"
+json_sub_id               = "id"
+json_sub_duration         = "subscription_duration"
+json_sub_price            = "price"
+json_sub_currency         = "currency"
+json_sub_start_date       = "start_date"
+json_sub_end_date         = "end_date"
+json_sub_type             = "subscription_type"
+
+
+json_user_preferences = "preferences"
+json_user_location    = "location"
+json_user_identity    = "identity"
+json_user_profile     = "profile"
+json_user_account     = "account"
+
+
+####################################################### Table Names
 table_matches       = "matches"
 table_blocks        = "blocks"
 table_likes         = "likes"
@@ -31,7 +62,11 @@ table_messages      = "messages"
 table_user_profile  = "user_profile"
 table_media         = "media"
 
-####################################################### shared column names
+table_prompts = "prompts"
+table_subscriptions = "subscriptions"
+
+
+####################################################### Column Names
 col_user_id  = "user_id"
 col_match_id = "match_id"
 col_like_id  = "like_id"
@@ -43,11 +78,10 @@ col_comment_message_id = "comment_message_id"
 col_conversation_span_minutes = "conversation_span_minutes"
 col_first_message_timestamp = "first_message_timestamp"
 
-####################################################### matches column names
+
 col_match_timestamp = "match_timestamp"
 
 
-####################################################### blocks column names
 col_block_id         = "block_id"
 col_block_timestamp  = "block_timestamp"
 col_block_type       = "block_type"
@@ -55,11 +89,9 @@ col_prompt_options   = "prompt_options"
 col_prompt_media_url = "prompt_media_url"
 col_selfie_verification = "selfie_verification"
 
-####################################################### likes column names
 col_like_timestamp  = "like_timestamp"
 
 
-####################################################### messages column names
 col_message_id        = "message_id"
 col_message_timestamp = "message_timestamp"
 col_message_body      = "body"
@@ -67,36 +99,12 @@ col_message_body      = "body"
 col_media_prompt = "media_prompt"
 
 
-
-####################################################### json keys (media)
-json_media_list     = "media"         # or whatever your file key is
-json_media_type     = "type"
-json_media_url      = "url"
-json_media_social   = "from_social_media"
-json_media_prompt = "prompt"
-
-
-####################################################### media columns
 col_media_id        = "media_id"
 col_media_url       = "media_url"
 col_media_type      = "media_type"
 col_media_social    = "from_social_media"
 
 
-
-####################################################### prompts json keys
-json_prompts        = "prompts"
-json_prompt_id      = "id"
-json_prompt_label   = "prompt"
-json_prompt_type    = "type"
-json_prompt_text    = "text"
-json_prompt_created = "created"
-json_prompt_updated = "user_updated"
-
-####################################################### table name
-table_prompts = "prompts"
-
-####################################################### prompts column names
 col_prompt_id         = "prompt_id"
 col_prompt_type       = "prompt_type"
 col_prompt_label      = "prompt"
@@ -105,29 +113,12 @@ col_prompt_created_ts = "created_timestamp"
 col_prompt_updated_ts = "updated_timestamp"
 
 
-####################################################### subscriptions json keys
-json_subscriptions        = "subscriptions"
-json_sub_id               = "id"
-json_sub_duration         = "subscription_duration"
-json_sub_price            = "price"
-json_sub_currency         = "currency"
-json_sub_start_date       = "start_date"
-json_sub_end_date         = "end_date"
-json_sub_type             = "subscription_type"
-
-####################################################### table name
-table_subscriptions = "subscriptions"
-
-####################################################### subscriptions
 col_subscription_id        = "subscription_id"
 col_subscription_start_ts  = "start_timestamp"
 col_subscription_end_ts    = "end_timestamp"
 col_subscription_currency  = "currency"
 col_subscription_type      = "subscription_type"
 col_subscription_price     = "price"
-
-
-####################################################### user profile
 
 
 col_upload_count = "upload_count"
@@ -139,9 +130,5 @@ col_profile      = "profile"
 col_account      = "account"
 
 
-json_user_preferences = "preferences"
-json_user_location    = "location"
-json_user_identity    = "identity"
-json_user_profile     = "profile"
-json_user_account     = "account"
+
 
