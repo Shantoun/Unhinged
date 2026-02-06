2d547531601b801ea6282e534dead73e1574bffb
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-02-06 11:20:32 -0500

    Add display of like match delay metrics

--- app.py
+++ app.py
@@ -733,7 +733,9 @@ if user_id:
             
             st.plotly_chart(fig_box_messaging_number, width="stretch")
 
-    
+            st.write(engagements[var.col_like_match_delay])
+            st.write(engagements[var.col_like_match_delay].mean())
+            
             mean_like_match_delay = int(engagements[var.col_like_match_delay].dropna().mean())
             fig_like_match_delay = viz.horizontal_boxplot(
                 engagements[var.col_like_match_delay],
