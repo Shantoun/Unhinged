e67f6c058c6d4b7d30c27e2b7565058957a9da56
Shantoun <40128978+Shantoun@users.noreply.github.com>
2026-02-07 21:14:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -547,9 +547,7 @@ if user_id:
             engagements_over_time = ds.events_over_time_df(engagements_copy, min_messages=convo_min_messages, min_minutes=convo_min_mins, join_comments_and_likes_sent=join_likes_comments, use_like_timestamp=use_like_time)
 
             ts_col_name = "Like Timestamp" if use_like_time else "Event Timestamp"
-            st.write(ts_col_name)
             engagements_over_time_filtered = filter.apply_date_filters(engagements_over_time, key="my_filter", date_col=ts_col_name, source_date_col=var.col_like_timestamp)
-            st.write(engagements_over_time_filtered)
             st.write("Date filters found:", [f for f in st.session_state.get("filters_my_filter", []) if f.get("column") == ts_col_name])
             
             fig_engagements_over_time, warning, output_df = viz.stacked_events_bar_fig(engagements_over_time_filtered)
